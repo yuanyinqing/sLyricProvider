993da10f7514eefa4f389534546e89cd857598f5
yuanjieyu <132210649+yuanjieyu@users.noreply.github.com>
2026-06-19 10:26:44 +0800

    chore(symfonium): bump version to 1.0.1 (versionCode 2)

--- symfonium/build.gradle.kts
+++ symfonium/build.gradle.kts
@@ -21,8 +21,8 @@ configure<ApplicationExtension> {
         applicationId = "io.github.proify.lyricon.symfoniumprovider"
         minSdk = 27
         targetSdk = rootProject.extra.get("targetSdkVersion") as Int
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
